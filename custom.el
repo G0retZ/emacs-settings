bda4bf6b92d4567cab804c43c59e876128c9b562
Sergei Mitrofanov <sergei.mitrofanov@onefootball.com>
2025-10-13 19:07:50 +0100

    Switch off auto-save

--- custom.el
+++ custom.el
@@ -4,6 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auto-save-default nil)
  '(column-number-mode t)
  '(completion-cycle-threshold 3)
  '(create-lockfiles nil)
